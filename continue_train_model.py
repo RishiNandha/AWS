834a27751c54351aeaab5b90894f5c86af5d9764
Rishi Nandha Vanchi <49914358+RishiNandha@users.noreply.github.com>
2022-07-21 22:34:01 +0530

    rwrtwe

--- continue_train_model.py
+++ continue_train_model.py
@@ -6,11 +6,6 @@ import pickle
 #import os
 #os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 
-x_train=pickle.load(open('x_train.bin','rb'))
-y_train=pickle.load(open('y_train.bin','rb'))
-
-x_val=pickle.load(open('x_val.bin','rb'))
-y_val=pickle.load(open('y_val.bin','rb'))
 
 print('\n\nLoaded Prepared Data Successfully')
 
@@ -34,6 +29,7 @@ layerz = [Conv2D(18, kernel_size=(5,5), input_shape=(160,160,3),activation='relu
 		Dense(6,activation='sigmoid')]
 
 model=keras.Sequential(layerz)
+[]
 model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(learning_rate=0.000005), metrics=['accuracy'])
 
 print('\n \nTraining: \n \n')
@@ -41,12 +37,24 @@ print('\n \nTraining: \n \n')
 checkpoint = keras.callbacks.ModelCheckpoint('ckpt.hdf5', monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
 stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=15)
 
-# model.load_weights('ckpt.hdf5')
-# USE ONLY IF CHECKPOINT EXISTS AND IS FROM THE SAME MODEL
+from keras.preprocessing.image import ImageDataGenerator
+# Generator Object with transformation settings
+train_datagen = ImageDataGenerator(
+        rescale=1./255,     shear_range=0.2,
+        zoom_range=0.2,     horizontal_flip=True,
+        vertical_flip=True, width_shift_range=0.2,
+        height_shift_range=0.2)
+# Vectorize Images in Training Directory
+train_generator = train_datagen.flow_from_directory(
+        'train',           target_size=(160, 160),
+        batch_size=32,      class_mode='categorical')
 
-model.fit(x_train, y_train, epochs=50, batch_size = 18, shuffle=True, callbacks=[checkpoint, stop], validation_data=(x_val, y_val))
+#Similarly generate validation set too called validation_generator
 
-model.load_weights('ckpt.hdf5')
+# Train Model
+model.fit_generator(
+        train_generator, steps_per_epoch=2000,
+        epochs=20)
 
 model.save("AWS")
 
